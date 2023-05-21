5d4e85326db590b6da5f09702847be2700fb7c37
Abin John <abinjohnanto@gmail.com>
2023-05-21 16:52:30 +0530

    init commit - make mockbee Vite compatible

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
+import Mockman from 'mockman-js';
+
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Routes>
+          <Route path="/mockman" element={<Mockman />} />
+        </Routes>
+      </div>
+    </>
+  );
+}
+
+export default App;
