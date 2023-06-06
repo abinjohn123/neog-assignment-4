c50d7446f9030d2ec0d504559f2c78092cd0138a
Abin John <abinjohnanto@gmail.com>
2023-06-06 14:37:48 +0530

    feat: add home and login to header

--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -8,7 +8,9 @@ const Header = () => {
   const navigate = useNavigate();
   return (
     <header className="app-header-container">
-      <div className="logo"></div>
+      <div className="logo" onClick={() => navigate('/')}>
+        kB
+      </div>
       <div className="search"></div>
       <div className="user-actions">
         {isLoggedIn ? (
@@ -20,9 +22,9 @@ const Header = () => {
               className="header-icon"
             >
               <path
-                fill-rule="evenodd"
+                fillRule="evenodd"
                 d="M7.5 6a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM3.751 20.105a8.25 8.25 0 0116.498 0 .75.75 0 01-.437.695A18.683 18.683 0 0112 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 01-.437-.695z"
-                clip-rule="evenodd"
+                clipRule="evenodd"
               />
             </svg>
           </div>
