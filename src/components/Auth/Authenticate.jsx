9998b956550836ac044256cc6a129a70bc9c4a58
Abin John <abinjohnanto@gmail.com>
2023-06-07 00:32:31 +0530

    feat: password check for new user

--- src/components/Auth/Authenticate.jsx
+++ src/components/Auth/Authenticate.jsx
@@ -2,9 +2,11 @@ import { useState } from 'react';
 import { useNavigate, useLocation } from 'react-router-dom';
 
 import { useAuth } from './useAuth';
+import { useSnackbar } from 'notistack';
 
 const Authenticate = ({ isNewUser = false }) => {
   const { signUp, logIn } = useAuth();
+  const { enqueueSnackbar } = useSnackbar();
   const [isSignup, setIsSignUp] = useState(isNewUser);
   const navigate = useNavigate();
   const location = useLocation();
@@ -25,6 +27,11 @@ const Authenticate = ({ isNewUser = false }) => {
     if (isSignup) {
       payload.firstName = e.target[3].value;
       payload.lastName = e.target[4].value;
+
+      if (e.target[1].value !== e.target[2].value) {
+        enqueueSnackbar('Passwords do not match!');
+        return;
+      }
       signUp(payload, successCallback);
     } else logIn(payload, successCallback);
   };
