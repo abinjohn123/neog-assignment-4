f2a1c61543a17b5fd23c8dc7acfd93f607288e7c
Abin John <abinjohnanto@gmail.com>
2023-06-07 00:18:20 +0530

    chore: update footer

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -2,16 +2,14 @@ const Footer = () => {
   return (
     <footer className="app-footer">
       <div>
-        <p>Some text</p>
-        <p>Some other text</p>
+        <a href="https://neog.camp/">neoG</a>
       </div>
       <div>
         <p>Made by Abin</p>
       </div>
-      <div>
-        <p>Instagram</p>
-        <p>GitHub</p>
-        <p>Twitter</p>
+      <div className="d-flex">
+        <a href="https://github.com/abinjohn123">GitHub</a>
+        <a href="https://abinjohn.in/twitter/">Twitter</a>
       </div>
     </footer>
   );
